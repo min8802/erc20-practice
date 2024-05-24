c9ecdebd5ed881504cd7d5d8d8dda72eed2651ae
min8802 <ww330@naver.com>
2024-05-24 15:00:19 +0900

    send function

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 import MetamaskButton from "./components/MetamaskButton";
 import Erc20Connect from "./components/ERC20Connect";
 import Balance from "./components/Balance";
+import { parseEther } from "ethers";
 
 const App = () => {
   const [signer, setSigner] = useState();
   const [contract, setContract] = useState();
   const [name, setName] = useState();
   const [symbol, setSymbol] = useState();
+  const [balance, setBalance] = useState(null);
+  const [transferAddress, setTransferAddress] = useState("");
+  const [transferAmount, setTransferAmount] = useState("");
 
   const getNameSymbol = async () => {
     try {
@@ -16,6 +20,17 @@ const App = () => {
 
       setName(nameResponse);
       setSymbol(symbolResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const onClickTransfer = async () => {
+    try {
+      if(!transferAddress || !transferAmount) return;
+
+      const nameResponse = await contract.transfer(transferAddress, parseEther(transferAmount, "wei"));
+      console.log(response);
     } catch (error) {
       console.error(error);
     }
@@ -36,7 +51,34 @@ const App = () => {
             0x3a3Fb2d14Ac142810Acc19cB981dE0B1BeB11cad
           </div>
           <Erc20Connect name={name} signer={signer} setContract={setContract} />
-          {name && <Balance name={name} symbol={symbol} contract={contract} />}
+          {name && 
+          <Balance name={name} 
+            symbol={symbol} 
+            contract={contract} 
+            balance={balance}
+            setBalance={setBalance}/>}
+
+          {balance && (
+                <div className="flex w-full items-end">
+                  <div>
+                    <input
+                      className="input-style w-full"
+                      type="text"
+                      placeholder="지갑주소"
+                      value={transferAddress}
+                      onChange={(e) => setTransferAddress(e.target.value)}
+                    />
+                    <input
+                      className="input-style w-full mt-4"
+                      type="text"
+                      placeholder="금액"
+                      value={transferAmount}
+                      onChange={(e) => setTransferAmount(e.target.value)}
+                    />
+                  </div>
+                  <button className="button-style ml-1" onClick={onClickTransfer}>Send</button>
+                </div>
+              )}
         </div>
       )}
     </div>
